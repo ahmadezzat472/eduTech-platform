4fa63b6063558e45bae644a9bffefe2763e76540
Ahmed Ezzat <admedo472@gmail.com>
2025-07-16 05:26:48 +0300

    fix viedo not load

--- src/pages/course/VideoDialog.tsx
+++ src/pages/course/VideoDialog.tsx
@@ -1,20 +1,33 @@
 import HeroVideoDialog from "@/components/magicui/hero-video-dialog";
 
+function getYouTubeEmbedUrl(url: string): string {
+  const videoIdMatch = url.match(
+    /(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=))([^&?]+)/
+  );
+  const videoId = videoIdMatch ? videoIdMatch[1] : null;
+  return videoId ? `https://www.youtube.com/embed/${videoId}` : url;
+}
+
 export default function VideoDialog({ videoUrl }: { videoUrl: string }) {
+  const embedUrl = getYouTubeEmbedUrl(videoUrl);
   return (
     <div className="relative">
       <HeroVideoDialog
         className="block dark:hidden"
         animationStyle="top-in-bottom-out"
-        videoSrc={videoUrl}
-        thumbnailSrc={videoUrl}
+        videoSrc={embedUrl}
+        thumbnailSrc={`https://img.youtube.com/vi/${embedUrl
+          .split("/")
+          .pop()}/hqdefault.jpg`}
         thumbnailAlt="Hero Video"
       />
       <HeroVideoDialog
         className="hidden dark:block"
         animationStyle="top-in-bottom-out"
-        videoSrc={videoUrl}
-        thumbnailSrc={videoUrl}
+        videoSrc={embedUrl}
+        thumbnailSrc={`https://img.youtube.com/vi/${embedUrl
+          .split("/")
+          .pop()}/hqdefault.jpg`}
         thumbnailAlt="Hero Video"
       />
     </div>
