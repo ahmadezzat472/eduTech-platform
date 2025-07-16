da3cbd1d6223b5dbb44ae9639c00bdb350bb427e
Ahmed Ezzat <admedo472@gmail.com>
2025-07-16 04:26:38 +0300

    add

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -3,51 +3,73 @@ import {
   TableBody,
   TableHead,
   TableHeader,
-  TableRow
+  TableRow,
 } from "@/components/ui/table";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Quiz } from "./Quiz"; // adjust the path if needed
+import { Quiz } from "./Quiz";
+import { useQuery } from "@tanstack/react-query";
+import { getTokenQuiz } from "@/services/quiz/getTokenQuiz";
+import Main from "@/components/common/Main";
 
-const userQuizzes = [
-  { id: 1, name: "Introduction to HTML", score: 100, level: "Beginner" },
-  { id: 2, name: "CSS Basics", score: 85, level: "Beginner" },
-  { id: 3, name: "JavaScript Fundamentals", score: 90, level: "Intermediate" },
-  { id: 4, name: "React Components", score: 75, level: "Intermediate" },
-  { id: 5, name: "Introduction to HTML", score: 100, level: "Advanced" },
-  { id: 6, name: "CSS Basics", score: 85, level: "Beginner" },
-  { id: 7, name: "JavaScript Fundamentals", score: 90, level: "Intermediate" },
-  { id: 8, name: "React Components", score: 75, level: "Intermediate" },
-];
+const UserQuizzes = () => {
+  const {
+    data: quizScores,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["userQuizScores"],
+    queryFn: getTokenQuiz,
+  });
 
-const index = () => {
+  if (isLoading)
+    return (
+      <div className="flex items-center justify-center h-[30vh]">
+        <div className="size-15 animate-spin rounded-full border-4 border-blue-600 border-t-transparent mx-auto" />
+      </div>
+    );
+  if (isError)
+    return (
+      <div className="text-center text-2xl font-bold text-red-500 flex items-center justify-center h-[50vh]">
+        Error loading quiz.
+      </div>
+    );
+  if (!quizScores)
+    return (
+      <div className="text-center text-2xl font-bold">No quiz data found.</div>
+    );
+
   return (
-    <Card className="max-w-5xl mx-auto my-10" id="user-quizzes">
-      <CardHeader>
-        <CardTitle className="text-2xl font-bold">Your Completed Quizzes</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-1/2">Quiz Name</TableHead>
-              <TableHead>Level</TableHead>
-              <TableHead className="text-right">Score</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {userQuizzes.map((quiz) => (
-              <Quiz
-                key={quiz.id}
-                name={quiz.name}
-                score={quiz.score}
-                level={quiz.level}
-              />
-            ))}
-          </TableBody>
-        </Table>
-      </CardContent>
-    </Card>
+    <Main>
+      <Card className="max-w-5xl mx-auto my-10" id="user-quizzes">
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold">
+            Your Completed Quizzes
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-1/2">Quiz Name</TableHead>
+                <TableHead>Level</TableHead>
+                <TableHead className="text-right">Score</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {quizScores?.map((quiz) => (
+                <Quiz
+                  key={quiz.category + quiz.level}
+                  name={quiz.category.split("-").join(" ").toUpperCase()}
+                  score={quiz.score}
+                  level={quiz.level}
+                />
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </Main>
   );
 };
 
-export default index;
+export default UserQuizzes;
