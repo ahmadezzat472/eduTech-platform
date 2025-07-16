70e5777066fda7e99d0f4de769ec5676456ba4ec
Ahmed Ezzat <admedo472@gmail.com>
2025-07-16 04:30:28 +0300

    add

--- src/pages/course/VideoDialog.tsx
+++ src/pages/course/VideoDialog.tsx
@@ -7,14 +7,14 @@ export default function VideoDialog({ videoUrl }: { videoUrl: string }) {
         className="block dark:hidden"
         animationStyle="top-in-bottom-out"
         videoSrc={videoUrl}
-        thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
+        thumbnailSrc={videoUrl}
         thumbnailAlt="Hero Video"
       />
       <HeroVideoDialog
         className="hidden dark:block"
         animationStyle="top-in-bottom-out"
         videoSrc={videoUrl}
-        thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
+        thumbnailSrc={videoUrl}
         thumbnailAlt="Hero Video"
       />
     </div>
